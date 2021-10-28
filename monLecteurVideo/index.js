cd1c5cb243b9aaf9e34cc86b0650afe8fca19a4d
Valentin <valentin.bordy@hotmail.com>
2021-10-28 17:04:21 +0200

    feat: progress bar

--- monLecteurVideo/index.js
+++ monLecteurVideo/index.js
@@ -6,10 +6,6 @@ const getBaseURL = () => {
 };
 
 let style = `
-video {
-    width: 50%;
-    height: 50%;
-}
 /* The Modal (background) */
 .modal {
   display: none; /* Hidden by default */
@@ -49,18 +45,71 @@ video {
   cursor: pointer;
 }
 
+figure {
+    box-shadow:
+      0 2.8px 2.2px rgba(0, 0, 0, 0.034),
+      0 6.7px 5.3px rgba(0, 0, 0, 0.048),
+      0 12.5px 10px rgba(0, 0, 0, 0.06),
+      0 22.3px 17.9px rgba(0, 0, 0, 0.072),
+      0 41.8px 33.4px rgba(0, 0, 0, 0.086),
+      0 100px 80px rgba(0, 0, 0, 0.12);
+    width: 65%;
+  }
+  
+  video {
+    display: block;
+    width: 100%;
+  }
+  progress {
+      width: 100%
+  }
+  
+  figcaption {
+    align-items: center;
+    background: #eaeaea;
+    display: grid;
+    grid-gap: 1rem;
+    /* play +10 vitessex4 volume progressbar timing info*/
+    grid-template-columns: 35px 50px 50px 50px 30px auto 50px 20px;
+    padding: .5rem;
+  }
+
+  button {
+    border: 0;
+    display: inline;
+    padding: .5rem;
+    transition: opacity .25s ease-out;
+    width: 100%;
+  }
+  
+  
 `;
+
 let template = /*html*/`
-  <video id="player">
+<figure>
+<video id="player">
   <br>  
   </video>
-  <br>
-  <button id="playPause">▶️</button>
-  <button id="info">GET INFO</button>
+  <figcaption>
+  <button aria-label="Play" id="playPause">▶️</button>
+  <button id="recule10">-10s</button>
   <button id="avance10">+10s</button>
-  <button id="vitesse4" >Vitesse 4x</button>
+  <select name="vitesse" id="vitesse-select">
+    <option value="0.5">x0.5</option>
+    <option value="1" selected="selected">x1</option>
+    <option value="2">x2</option>
+    <option value="4">x4</option>
+  </select>
   <webaudio-knob id="volume" min=0 max=1 value=0.5 step="0.01" 
          tooltip="%s" diameter="20" src="./assets/Aqua.png" sprites="100"></webaudio-knob>
+
+  <progress id="progress" max="100" value="0">Progress</progress>
+  <label id="timer" for="progress" role="timer">0min0</label>
+  <button id="info">ℹ️</button>
+
+  </figcaption>
+</figure>
+  <br>
 
 
 
@@ -130,8 +179,11 @@ class MyVideoPlayer extends HTMLElement {
         this.shadowRoot.querySelector("#avance10").onclick = () => {
             this.player.currentTime += parseFloat("10.0");
         }
-        this.shadowRoot.querySelector("#vitesse4").onclick = () => {
-            this.player.playbackRate = 4;
+        this.shadowRoot.querySelector("#recule10").onclick = () => {
+            this.player.currentTime -= parseFloat("10.0");
+        }
+        this.shadowRoot.querySelector("#vitesse-select").onclick = (event) => {
+            this.player.playbackRate = this.shadowRoot.querySelector("#vitesse-select").value;
         }
         this.shadowRoot.querySelector("#info").onclick = (event) => {
             let minutes = Math.floor(this.player.duration / 60);
@@ -147,7 +199,9 @@ class MyVideoPlayer extends HTMLElement {
                 '<b><br> Lien de la vidéo : </b>' + this.player.currentSrc;
             this.openModal(event);
         }
-
+        this.shadowRoot.querySelector("#player").onplay = () => {
+            this.progressLoop();
+        }
         this.shadowRoot.querySelector("#volume").oninput = (event) => {
             const vol = parseFloat(event.target.value);
             this.player.volume = vol;
@@ -182,11 +236,25 @@ class MyVideoPlayer extends HTMLElement {
         }
     }
 
-    // When the user clicks the button, open the modal 
     openModal(event) {
         this.shadowRoot.querySelector("#myModal").style.display = "block";
         isModalOpen = true;
         event.stopPropagation();
+    }
+
+    progressLoop() {
+        let progress = this.shadowRoot.querySelector("#progress");
+        let timer = this.shadowRoot.querySelector("#timer");
+
+        setInterval(() => {
+
+            let minutes = Math.floor(this.player.currentTime / 60);
+            let seconds = this.player.currentTime - minutes * 60;
+
+            progress.value = Math.round((this.shadowRoot.querySelector("#player").currentTime / this.shadowRoot.querySelector("#player").duration) * 100);
+            timer.innerHTML = minutes + "min" +
+                Math.round(seconds);
+        });
     }
 
     // API de mon composant
